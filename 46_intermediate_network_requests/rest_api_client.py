9c15c5bd7e5a370a8d58c519820ea0f073040982
Dylan Cruz <dylan@dylancruz.me>
2025-02-23 15:05:35 -0800

    chao 46

---
+++ 46_intermediate_network_requests/rest_api_client.py
@@ -0,0 +1,115 @@
+import requests
+import json
+
+while True:
+    print('\n- Web Store -')
+    print('1. Get Product Info')
+    print('2. Delete Product')
+    print('3. Add Product')
+    print('4. Update Product')
+    menu_selection = int(input('\nEnter your selection: '))
+
+    if menu_selection == 1:
+        while True:
+            all_products_json = requests.get('http://localhost:8000/products').json()
+
+            print(f'\n- All Products -')
+            for x in range(0, len(all_products_json)):
+                print(f'{x + 1}. ' + all_products_json[x]['name'])
+            print(f'{len(all_products_json) + 1}. Return')
+
+            menu_selection = int(input('\nSelect a product to get more information: '))
+            if menu_selection == (len(all_products_json) + 1):
+                break
+
+            elif menu_selection <= len(all_products_json):
+                get_url = 'http://localhost:8000/products/' + all_products_json[menu_selection-1]['name']
+                product_json = requests.get(get_url).json()
+                print(f'\nProduct Name: {product_json['name']}')
+                print(f'Stock: {product_json['stock']}')
+                print(f'Price: {product_json['price']}')
+
+            else:
+                print('Invalid input!')
+
+    elif menu_selection == 2:
+        while True:
+            all_products_json = requests.get('http://localhost:8000/products').json()
+
+            print(f'\n- All Products -')
+            for x in range(0, len(all_products_json)):
+                print(f'{x + 1}. ' + all_products_json[x]['name'])
+            print(f'{len(all_products_json) + 1}. Return')
+
+            menu_selection = int(input('\nSelect a product to delete: '))
+            if menu_selection == (len(all_products_json) + 1):
+                break
+
+            elif menu_selection <= len(all_products_json):
+                product_url = 'http://localhost:8000/products/' + all_products_json[menu_selection-1]['name']
+                requests.delete(product_url)
+
+    elif menu_selection == 3:
+        new_product_name = input('\nEnter the products name: ')
+        new_product_stock = int(input('Enter the stock of the product: '))
+        new_product_price = int(input('Enter the price of the product: $'))
+
+        request_data = {
+            'name': new_product_name,
+            'stock': new_product_stock,
+            'price': new_product_price,
+        }
+
+        response = requests.post('http://localhost:8000/products/',
+                                 data=json.dumps(request_data),
+                                 headers={'Content-Type': 'application/json'})
+
+        print('\nProduct created successfully')
+
+    elif menu_selection == 4:
+        while True:
+            all_products_json = requests.get('http://localhost:8000/products').json()
+
+            print(f'\n- All Products -')
+            for x in range(0, len(all_products_json)):
+                print(f'{x + 1}. ' + all_products_json[x]['name'])
+            print(f'{len(all_products_json) + 1}. Return')
+
+            menu_selection = int(input('\nSelect a product to update: '))
+            if menu_selection == (len(all_products_json) + 1):
+                break
+
+            elif menu_selection <= len(all_products_json):
+                product = all_products_json[menu_selection-1]
+                print(f'\nUpdating {product['name']}. Choose a field to update:')
+                menu_selection = int(input('1. Name | 2. Stock | 3. Price | : '))
+
+                if menu_selection == 1:
+                    product_new_name = input('Enter a new name for the product: ')
+                    request_data = {'new_value': product_new_name, 
+                                    'name': product['name'], 
+                                    'update_value': 'name'}
+
+                elif menu_selection == 2:
+                    product_new_stock = int(input('Enter a new stock for the product: '))
+                    request_data = {'new_value': product_new_stock, 
+                                    'name': product['name'], 
+                                    'update_value': 'stock'}
+
+                elif menu_selection == 3:
+                    product_new_price = int(input('Enter a new price for the product: '))
+                    request_data = {'new_value': product_new_price, 
+                                    'name': product['name'], 
+                                    'update_value': 'price'}
+                 
+                else:
+                    print('Invalid Input!')
+                    continue
+                
+                response = requests.put('http://localhost:8000/products/',
+                                            data=json.dumps(request_data),
+                                            headers={'Content-Type': 'application/json'})
+                print('\nProduct updated successfully')
+                    
+    else:
+        print('Invalid input!')
---
+++ 46_intermediate_network_requests/rest_api_server.py
@@ -0,0 +1,76 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+import urllib.parse
+
+products = [
+    { 'name': 'Headphones', 'stock': 50, 'price': 200},
+    { 'name': 'Computer', 'stock': 10, 'price': 1000},
+    { 'name': 'Pen', 'stock': 100, 'price': 5},
+]
+
+class RESTAPIHandler(BaseHTTPRequestHandler):
+    def do_GET(self):        
+        path_segments = urllib.parse.urlparse(self.path).path.split('/')
+
+        if path_segments[1] == 'products':
+            if len(path_segments) == 2:
+                params = urllib.parse.parse_qs(urllib.parse.urlparse(self.path).query)
+                name_filter = params.get('name', [''])[0]
+
+                if len(name_filter) > 0:
+                    response_data = json.dumps(
+                        [p for p in products if name_filter in p['name']]
+                    ).encode()
+                else:
+                    response_data = json.dumps(products).encode()
+                
+                self.send_response(200)
+                self.send_header('Content-Type', 'application/json')
+                self.send_header('Content-Length', len(response_data))
+                self.end_headers()
+                self.wfile.write(response_data)
+            else:
+                product = next((product for product in products if product['name'] == path_segments[2]))
+                response_data = json.dumps(product).encode()
+                self.send_response(200)
+                self.send_header('Content-Type', 'application/json')
+                self.send_header('Content-Length', len(response_data))
+                self.end_headers()
+                self.wfile.write(response_data)
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        request_data = self.rfile.read(content_length)
+        new_product = json.loads(request_data.decode())
+        products.append(new_product)
+        self.send_response(201)
+        self.end_headers()
+
+    def do_PUT(self):
+        content_length = int(self.headers['Content-Length'])
+        request_data = self.rfile.read(content_length)
+        product_update_data = json.loads(request_data.decode())
+
+        for i in range(0, len(products)):
+            if products[i]['name'] == product_update_data['name']:
+                products[i][product_update_data['update_value']] = product_update_data['new_value']
+                break
+
+        self.send_response(201)
+        self.end_headers()
+
+    def do_DELETE(self):
+        path_segments = self.path.split('/')
+
+        if path_segments[1] == 'products':
+            product = next((product for product in products if product['name'] == path_segments[2]))
+            self.send_response(200)
+            self.end_headers()
+            for x in range(0, len(products)):
+                if products[x]['name'] == product['name']:
+                    del products[x]
+                    break
+
+server = HTTPServer(('localhost', 8000), RESTAPIHandler)
+print('Server is listening on localhost:8000')
+server.serve_forever()
