b7be9bf119cc8b1b55fc0d906987290cbf3a1973
Dylan Cruz <dylan@dylancruz.me>
2025-02-25 20:00:44 -0800

    finished chap 47

--- 47_understand_socket_programming/socket_client.py
+++ 47_understand_socket_programming/socket_client.py
@@ -1,20 +1,34 @@
 import socket
+import threading
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = socket.gethostname()
-port = 8000
+port = 8001
 
 client_socket.connect((host, port))
 
+
+
 # Send message to server
 
-while True:
-    next_message = input('Please enter the next message you want to to send the server: ')
-    client_socket.sendall(next_message.encode('utf-8'))
-    data = client_socket.recv(1024).decode('utf-8')
-    print(f'The server responded with: {data}')
-    if next_message == 'done':
-        break
+def start_input_loop():
+    while True:
+        next_message = input('Please enter the next message you want to to send the server: ')
+        client_socket.sendall(next_message.encode('utf-8'))
+        data = client_socket.recv(1024).decode('utf-8')
+        if next_message == 'done':
+            break
 
-client_socket.close()
+    client_socket.close()
+
+input_loop = threading.Thread(target=start_input_loop)
+input_loop.start()
+
+def listen_for_messages():
+    while True:
+        data = client_socket.recv(1024).decode('utf-8')
+        print(f'\nNew Message!: {data}')
+
+messages_loop = threading.Thread(target=listen_for_messages)
+messages_loop.start()
--- 47_understand_socket_programming/socket_server.py
+++ 47_understand_socket_programming/socket_server.py
@@ -1,27 +1,35 @@
 import socket
+import threading
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clients = []
 
 host = socket.gethostname()
-port = 8000
+port = 8001
 
 print(f'Waiting for connections on {host}:{port}')
 
 server_socket.bind((host, port))
 server_socket.listen(3)
 
-client_socket, client_address = server_socket.accept()
-print(f'A new client has connected, with address {client_address}')
+def handle_client(client_socket):
+    global clients
+    
+    while True:
+        data = client_socket.recv(1024).decode('utf-8')
+        print(f'The client says: {data}')        
+        for client in clients:
+            client.sendall(data.encode('utf-8'))
 
-# Listen for message, and respond to client
+        if data == 'done':
+            break
+    
+    client_socket.close()
+    clients.remove(client_socket)
 
 while True:
-    data = client_socket.recv(1024).decode('utf-8')
-
-    print(f'The client says: {data}')
-    client_socket.sendall(f'I received your message: {data}'.encode('utf-8'))
-    
-    if data == 'done':
-        break
-
-client_socket.close()
+    client_socket, client_address = server_socket.accept()
+    clients.append(client_socket)
+    print(f'A new client has connected, with address {client_address}')
+    new_thread = threading.Thread(target=handle_client, args=(client_socket,))
+    new_thread.start()
