ef9f7e70afdef8e72fba8ad7b5986baab2dba679
Dylan Cruz <dylan@dylancruz.me>
2025-03-05 12:55:13 -0800

    started chap 50

---
+++ 50_understand_the_xml_format/working_with_xml.py
@@ -0,0 +1,18 @@
+import xml.etree.ElementTree as ET
+
+tree = ET.parse("recipes.xml")
+root = tree.getroot()
+
+print(f'The root has the tag {root.tag}')
+print(f'The recipe was published in {root.attrib["published"]}')
+
+def print_element(element):
+    print(element.tag)
+    if element.text and not element.text.isspace():
+        print(element.text)
+    if 'name' in element.attrib:
+        print(element.attrib["name"])
+    for child in element:
+        print_element(child)
+
+print_element(root)
